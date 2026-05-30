d47658dc14e366027a76770b17d18ab502c4b928
colpie34 <imbillubosdmosh@gmail.com>
2026-05-30 21:01:10 +0530

    Initial Tic Tac Toe game
    # Tic-Tac-Toe
    
    A simple two-player Tic-Tac-Toe game written in Python.
    
    ## Features
    - Two-player gameplay
    - Input validation
    - Tie detection
    - Magic square win detection
    
    ## Run
    
    python main.py

--- main.py
+++ main.py
@@ -1,68 +1,78 @@
-def TicTacToe():
-  board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  end = False
-  MagicSquare = [4, 9, 2, 3, 5, 7, 8, 1, 6]
+def tic_tac_toe():
+    """A two-player console Tic Tac Toe game using a magic square for win detection."""
 
-  def PrintBoard():
-    print()
-    print('', board[0], "|", board[1], "|", board[2])
-    print("---|---|---")
-    print('', board[3], "|", board[4], "|", board[5])
-    print("---|---|---")
-    print('', board[6], "|", board[7], "|", board[8])
-    print()
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    magic_square = [4, 9, 2, 3, 5, 7, 8, 1, 6]
 
-  def GetNumber():
+    def print_board():
+        print()
+        print(f" {board[0]} | {board[1]} | {board[2]}")
+        print("---|---|---")
+        print(f" {board[3]} | {board[4]} | {board[5]}")
+        print("---|---|---")
+        print(f" {board[6]} | {board[7]} | {board[8]}")
+        print()
+
+    def get_number():
+        while True:
+            try:
+                number = int(input("Enter a position (1-9): "))
+                if 1 <= number <= 9:
+                    return number
+                print("Number must be between 1 and 9.")
+            except ValueError:
+                print("Please enter a valid number.")
+
+    def turn(player):
+        while True:
+            position = get_number() - 1
+
+            if board[position] in ["X", "O"]:
+                print("That position is already occupied.")
+            else:
+                board[position] = player
+                break
+
+    def check_win(player):
+        for x in range(9):
+            for y in range(9):
+                for z in range(9):
+                    if x != y and y != z and z != x:
+                        if (
+                            board[x] == player
+                            and board[y] == player
+                            and board[z] == player
+                            and magic_square[x]
+                            + magic_square[y]
+                            + magic_square[z]
+                            == 15
+                        ):
+                            print_board()
+                            print(f"Player {player} wins!")
+                            return True
+
+        if all(space in ["X", "O"] for space in board):
+            print_board()
+            print("The game ends in a tie!")
+            return True
+
+        return False
+
     while True:
-      number = input()
-      try:
-        number  = int(number)
-        if number in range(1, 10):
-          return number
-        else:
-          print("\nNumber not on board")
-      except ValueError:
-        print("\nThat's not a number. Try again")
-        continue
+        print_board()
+        print("Player X's turn")
+        turn("X")
 
-  def Turn(player):
-    placing_index = GetNumber() - 1
-    if board[placing_index] == "X" or board[placing_index] == "O":
-      print("\nBox already occupied. Try another one")
-      Turn(player)
-    else:
-      board[placing_index] = player
+        if check_win("X"):
+            break
 
-  def CheckWin(player):
-    count = 0
+        print_board()
+        print("Player O's turn")
+        turn("O")
 
-    for x in range(9):
-      for y in range(9):
-        for z in range(9):
-          if x != y and y != z and z != x:
-            if board[x] == player and board[y] == player and board[z] == player:
-              if MagicSquare[x] + MagicSquare[y] + MagicSquare[z] == 15:
-                print("Player", player ,"wins!\n")
-                return True
+        if check_win("O"):
+            break
 
-    for a in range(9):
-      if board[a] == "X" or board[a] == "O":
-        count += 1
-      if count == 9:
-        print("The game ends in a Tie\n")
-        return True
 
-  while not end:
-    PrintBoard()
-    end = CheckWin("O")
-    if end == True:
-      break
-    print("Choose a box player X")
-    Turn("X")
-
-    PrintBoard()
-    end = CheckWin("X")
-    if end == True:
-      break
-    print("Choose a box player O")
-    Turn("O")
+if __name__ == "__main__":
+    tic_tac_toe()
